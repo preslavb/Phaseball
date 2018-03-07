ad6801d6d98b0b29325a55b332d2d03df79eac7a
Shayder11 <shaydergaming@gmail.com>
2018-03-07 17:49:53 +0200

    reset

--- Assets/ScoreControll.cs
+++ Assets/ScoreControll.cs
@@ -9,6 +9,7 @@ public class ScoreControll : MonoBehaviour {
 
 	public GameObject Goal;
 	public GameObject Goal1;
+    public GameObject Ball;
 	private int scoreText1;
 	private int scoreText2;
 
@@ -31,20 +32,20 @@ public class ScoreControll : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		int playerNumberWhoScored = 0;
+		//int playerNumberWhoScored = 0;
 
 	  
-		 if (Goal.gameObject.tag == "Ball") 
-	    {
-		 scoreText1++;
+		// if (Goal.gameObject.tag == "Ball") 
+	   // {
+		// scoreText1++;
 			
-	    }
+	   // }
 
-	    if (Goal1.gameObject.tag == "Ball")
-	    {
-		  scoreText2++;
+	   // if (Goal1.gameObject.tag == "Ball")
+	  //  {
+		//  scoreText2++;
 			
-	    }
+	   // }
 
 	  
 
@@ -52,31 +53,42 @@ public class ScoreControll : MonoBehaviour {
 
 		Debug.Log("Collision Detected");
 
-		
-	   print(player1Score + " - " + player2Score);
 
-		
-	}
+        // print(player1Score + " - " + player2Score);
+
+
+        if (Ball.gameObject.tag == "Goal1")
+        {
+            transform.position = gameObject.transform.position;
+        }
+
+
+
+        if (Ball.gameObject.tag == "Goal")
+        {
+            transform.position = gameObject.transform.position;
+        }
+    }
 
    
 
    
 
 
-	void OnTriggerEnter(Collider coll)
+	void OnTriggerEnter2D(Collider coll)
 	{
-		//if(coll.gameObject.tag == "Goal1")
-		//{
-		//	scoreText1 = scoreText1 + 1;
-		//}
+		if(Ball.gameObject.tag == "Goal1")
+		{
+			scoreText1 = scoreText1 + 1;
+		}
 
-		//if(coll.gameObject.tag == "Goal")
-		//{
-		//	scoreText2 = scoreText2 + 1;
-		//}
+		if(Ball.gameObject.tag == "Goal")
+		{
+			scoreText2 = scoreText2 + 1;
+		}
 
-		scoreText1 = scoreText1 + 1;
-		scoreText2 = scoreText2 + 1;
+		//scoreText1 = scoreText1 + 1;
+		//scoreText2 = scoreText2 + 1;
 	}
 
 	
