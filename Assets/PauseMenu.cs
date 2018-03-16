642e43a0d3c5f0781d21cbd3cf5353186155e796
Shayder11 <shaydergaming@gmail.com>
2018-03-16 19:57:57 +0200

    pause menu
    pause resume function quit function doesnt work atm because i dont have
    a main menu scene yet

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class PauseMenu : MonoBehaviour {
+	public Platformer2DUserControl playerController; 
+	public Transform image;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Joystick Button 7"))
+			{
+			PauseGame ();
+			}
+	}
+	public void PauseGame()
+	{
+		if(image.gameObject.activeInHierarchy == false)
+		{
+			image.gameObject.SetActive(true);
+			Time.timeScale = 0;
+		}
+		else 
+		{
+			image.gameObject.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+}
