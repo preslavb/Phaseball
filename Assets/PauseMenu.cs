27bfc93d8fbfce3e77150e54e72bca350c29e3e0
Shayder11 <shaydergaming@gmail.com>
2018-03-17 13:33:08 +0200

    options / quit
    options and quit buttons have functions

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -5,8 +5,8 @@ using UnityStandardAssets._2D;
 
 public class PauseMenu : MonoBehaviour {
 	public Platformer2DUserControl playerController; 
-	public Transform image;
-
+	public Transform pauseMenu;
+	public Transform optionsMenu;
 
 	// Use this for initialization
 	void Start () {
@@ -22,15 +22,55 @@ public class PauseMenu : MonoBehaviour {
 	}
 	public void PauseGame()
 	{
-		if(image.gameObject.activeInHierarchy == false)
+		if(pauseMenu.gameObject.activeInHierarchy == false)
 		{
-			image.gameObject.SetActive(true);
+			pauseMenu.gameObject.SetActive(true);
+			optionsMenu.gameObject.SetActive (false);
 			Time.timeScale = 0;
 		}
 		else 
 		{
-			image.gameObject.SetActive(false);
+			pauseMenu.gameObject.SetActive(false);
+			optionsMenu.gameObject.SetActive (false);
 			Time.timeScale = 1;
 		}
+	}
+
+	public void Options(bool Selected)
+	{
+		if (Selected) 
+		{
+			optionsMenu.gameObject.SetActive (true);
+			pauseMenu.gameObject.SetActive(false);
+
+		}
+		if (!Selected) 
+		{
+			optionsMenu.gameObject.SetActive (false);
+			pauseMenu.gameObject.SetActive (true);
+
+		}
+	}
+
+	public void Back(bool Clicked)
+	{
+		if (Clicked)
+		{
+			optionsMenu.gameObject.SetActive (false);
+			pauseMenu.gameObject.SetActive(true);
+
+		}
+
+		if (!Clicked) 
+		{
+			optionsMenu.gameObject.SetActive (true);
+			pauseMenu.gameObject.SetActive(false);
+
+		}
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
 	}
 }
